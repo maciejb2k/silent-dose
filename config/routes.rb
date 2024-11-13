033d41db7f6d0f1a3101fbbb55f45bba5b2e7309
Maciej Biel <maciek.b2k@gmail.com>
2024-11-13 17:37:14 +0100

    refactor: change routes.rb to restrict access to Sidekiq web interface for admin users only

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  mount Sidekiq::Web => "/sidekiq"
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
